aa9d5408d06f4538b6655539469024345dc69f61
gczajka <gczajkajv@gmail.com>
2019-12-28 09:47:51 +0100

    test commit

---
+++ src/main/java/com/frontend/rentacarfd/RentACarFdApplication.java
@@ -0,0 +1,13 @@
+package com.frontend.rentacarfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarFdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarFdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frontend/rentacarfd/RentACarFdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend.rentacarfd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarFdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
