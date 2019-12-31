2bf0a2e7148b30570d1f954b4475db423642c5e8
gczajka <gczajkajv@gmail.com>
2019-12-31 15:42:54 +0100

    added CarClient, setItems for Grid, CoreConfig

--- src/main/java/com/frontend/rentacarfd/views/car/CarView.java
+++ src/main/java/com/frontend/rentacarfd/views/car/CarView.java
@@ -1,21 +1,29 @@
 package com.frontend.rentacarfd.views.car;
 
+import com.frontend.rentacarfd.client.CarClient;
 import com.frontend.rentacarfd.domain.CarDto;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CarView extends VerticalLayout {
     private Grid<CarDto> grid = new Grid<>(CarDto.class);
+    private CarClient carClient;
 
-    public CarView() {
-        Button button1 = new Button("Click1");
-        Button button2 = new Button("Click2");
+    public CarView(@Autowired CarClient carClient) {
+        this.carClient = carClient;
 
+        Button button1 = new Button("Add new car");
         grid.setColumns("id", "brand", "model", "colour", "engineType", "engineCapacity", "productionYear", "costPerDay", "available");
+        List<CarDto> cars = carClient.getCars();
+        grid.setItems(cars);
 
-        add(button1, button2, grid);
+        add(button1, grid);
     }
 }
